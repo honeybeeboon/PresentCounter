19ab94e3a69e19c05c95a9ccb951f03b36851141
kazuki <honeybee.zzz119@gmail.com>
2020-08-15 12:06:04 +0900

    Initial Commit

---
+++ PresentCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PresentCounter
+//
+//  Created by 下澤一輝 on 2020/08/15.
+//  Copyright © 2020 下澤一輝. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PresentCounterTests/PresentCounterTests.swift
@@ -0,0 +1,34 @@
+//
+//  PresentCounterTests.swift
+//  PresentCounterTests
+//
+//  Created by 下澤一輝 on 2020/08/15.
+//  Copyright © 2020 下澤一輝. All rights reserved.
+//
+
+import XCTest
+@testable import PresentCounter
+
+class PresentCounterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
